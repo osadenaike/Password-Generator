f0b585175fd441a212126a0feadff65f21177150
osadenaike <oluz@Oluwafemis-Air.attlocal.net>
2020-06-13 17:15:54 -0500

    included initial prompt messages

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Assignment code here
 
+// Created all variabes needed
 var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var number = [0,1,2,3,4,5,6,7,8,9];
-var speccharacter = ['!','#','$','%','&','*','+','-','.','/',':',';','<','=','>','?','@','^','_','~'];
+var specialcharacter = ['!','#','$','%','&','*','+','-','.','/',':',';','<','=','>','?','@','^','_','~'];
 var result = []; 
 
 // Get references to the #generate element
@@ -11,6 +12,13 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var Passwordlength= window.prompt("Password needs to be between 8-128 characters in length.");
+  if (Passwordlength < 8 || Passwordlength > 128) {
+    window.alert("Password needs to be between 8-128 charcters. Please try again.");
+      return writePassword();
+  };
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
